09275b37069fa826c75ef6e92fc8763895b6a3ff
Rahulji.V <rahuljiv2004@gmail.com>
2025-11-03 11:18:00 +0530

    named to chennai

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello from Shiv Nadar University Chennai!</h1><p>Welcome to our web application.</p>made with ❤️ by Rahulji</p>');
-});
+    res.send('<h1>Hello from Chennai!</h1><p>Welcome to our web application.</p>made with ❤️ by Rahulji</p>');
+})
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'healthy', timestamp: new Date() });
